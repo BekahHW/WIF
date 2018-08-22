e9e96a65512db96f0eeb25fb95a87885d45f5ad7
BekahHW <rhawrot@gmail.com>
2018-08-21 23:08:02 -0400

    Create wif.rb file

---
+++ lib/wif.rb
@@ -0,0 +1,13 @@
+require_relative './wif/version'
+
+module Wif
+  # Your code goes here...
+end
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require_relative './wif/cli'
+require_relative './wif/filmography'
+require_relative './wif/scraper'
+require_relative './wif/version'
