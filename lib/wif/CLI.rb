c3fef411c18cde4f819cd248daf2266b8623dc85
BekahHW <rhawrot@gmail.com>
2018-08-22 22:17:51 -0400

    Create list method in CLI

--- lib/wif/CLI.rb
+++ lib/wif/CLI.rb
@@ -8,4 +8,14 @@ class Wif::CLI
     Wif::Scraper.scrape_heroines
     list
   end
+
+  def list
+    puts "Here is a list of ten movies with kickass heroines:"
+
+    movies = Kaw::Filmography.all.sample(10)
+    movies.each.with_index(1) do |movie, i|
+      puts "#{i}. #{movie.title}"
+    end
+    select_list(movies)
+  end
 end
