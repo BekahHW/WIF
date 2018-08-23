dbb5b583519d7f9cc238be7cf58e77215dc08070
BekahHW <rhawrot@gmail.com>
2018-08-22 22:17:07 -0400

    Created start method in CLI class

--- lib/wif/CLI.rb
+++ lib/wif/CLI.rb
@@ -1,4 +1,11 @@
 class Wif::CLI
 
-
+  def start
+    puts "Welcome to Women in film!"
+    puts ""
+    puts "Let's get started with a sample of movies with amazing female characters."
+    puts ""
+    Wif::Scraper.scrape_heroines
+    list
+  end
 end
