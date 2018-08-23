fbb0d537adf4a5ccdda68e803cd3cef95a021efc
BekahHW <rhawrot@gmail.com>
2018-08-22 21:58:29 -0400

    Add a Base_url and scrape_heroine method to the Scraper class

--- lib/wif/Scraper.rb
+++ lib/wif/Scraper.rb
@@ -1,3 +1,13 @@
 class Wif::Scraper
 
+  BASE_URL = "https://editorial.rottentomatoes.com/article/fearless-female-movie-heroes-who-inspire-us/"
+
+  def self.scrape_heroines
+    doc = Nokogiri::HTML(open(BASE_URL))
+
+    doc.search(".articleContentBody .clearfix").each do |each_movie|
+
+      movie = Wif::Filmography.new
+    end
+  end
 end
