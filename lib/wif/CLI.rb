0919e3296f38b07736e99018e9472bce2e2c2c34
BekahHW <rhawrot@gmail.com>
2018-08-22 23:19:13 -0400

    Eliminate unnecessary spacing and capitalize title of project

--- lib/wif/CLI.rb
+++ lib/wif/CLI.rb
@@ -1,18 +1,16 @@
 class Wif::CLI
 
   def start
-    puts "Welcome to Women in film!"
+    puts "Welcome to Women in Film!"
     puts ""
     puts "Let's get started with a sample of movies with amazing female characters."
     puts ""
     Wif::Scraper.scrape_heroines
     puts "Here is a list of ten movies with kickass heroines:"
-
     list
   end
 
   def list
-
     movies = Wif::Filmography.all.sample(10)
     movies.each.with_index(1) do |movie, i|
       puts "#{i}. #{movie.title}"
